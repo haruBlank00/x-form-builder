d089def094c143e3814458fe4d938959148bf384
haruBlank <harublank00@gmail.com>
2025-02-01 17:08:37 +0545

    feat: setup and install tailwind css and shadcn

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
     <div>
       <h1>Home sweet home.</h1>
       <p>Now go build something great.</p>
       <p>Starting with a form </p>
+      <Button>Let's goooo</Button>
     </div>
   );
 }
